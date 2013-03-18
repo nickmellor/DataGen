443a42d1e85d6f80632689d7d38a24b27e8e98bd
nickmellor <ferenc>
2013-03-19 02:01:04 +1100

    input and output translations, all unit tests pass

--- randomPerson.py
+++ randomPerson.py
@@ -4,17 +4,16 @@ __author__ = 'nick'
 #########################     Version 1.2 alpha   ###################################
 #########################      Nov 2012           ###################################
 """Random Personal Data Generator
-Generates random details of fictitious people (for testing purposes?)
+Generates random details of fictitious people by obfuscating an existing
+address list and randomly choosing new names and personal characteristics
 
 Includes:
 full name, sex, birthdate, address, web address,
 email address etc. based on an existing contact list, name popularity data and
 basic demographics
 
-Generates CSV file, YAML file (Django fixture) or a continuous stream of realistic
-but random personal data in Python dictionary format
-
-Random data is based on obfuscated real-world addresses
+Generates CSV file, YAML file (Django fixture) or a continuous stream of people
+in Python dictionaries
 
 An ordinary contact list can be dropped into the generator unchanged
 to act as model data-- you can use your own address book to generate realistic
@@ -23,7 +22,8 @@ modified for other sources
 
 Transformations:
 
-Typically
+Typically your address database and your target database differ in format. NameGen
+can take one set of field names and transform them into another. See fieldmap.py
 
 Obfuscations include:
   - all numbers in first line of address are changed
@@ -33,7 +33,7 @@ Obfuscations include:
   - birthdays are randomised (realistically)
 
 Performance:
-1000+ people per second (2GHz laptop, 200 entries in address file)
+1000+ people per second (2GHz laptop, 200 entries in input address file)
 
 Dependencies:
 pyyaml for test framework
@@ -45,14 +45,13 @@ Future (Nov 2012):
 - With the right datafiles, could mix names from different nationalities or demographics
   (e.g. software developers, middle managers, single parents) and/or generate data for other
   purposes such as organisation names, job titles, fruit and vegetable orders etc.
-- remove string literals from code (e.g. "First name")
 - Better unit testing
 
 Updates:
 Nov 2012-- unit tests, minor refactorings and code commenting
 Jan 2013-- began work on output transformations (selecting and renaming of data
            fields
-
+Mar 2013-- input and output transformations in place
 
 
 """
@@ -68,8 +67,9 @@ import bisect
 from filelinks import lookup_file, output_file, base_dir
 
 # Todo-- pluggable data generators
-# (e.g. generate a database of young female entrepreneurs, a male-heavy corporate
-# population or an Asian state in which there are fewer females than males
+# (e.g. generate a posse of young women entrepreneurs, a corporation
+# employing mainly male building contractors, an Asian state in which there are
+# fewer women, or generate families (couples, single or double-parent, 1-5 children etc)
 
 class RandomPersonException(BaseException):
     """
@@ -198,8 +198,9 @@ class RandomPerson:
         # Input address data might have nothing in common with
         # output address data order
         fieldorder = csv.reader(open(filename)).next()
-        fieldorder = [fieldmap.INCOMING_TRANSLATION[r] for r in fieldorder]
-        self.fieldorder = [r for r in fieldorder if not r]
+        fieldorder = dict(zip(fieldorder, fieldorder))
+        fieldorder = fieldmap.translateIn(fieldorder)
+        self.fieldorder = [r for r in fieldorder if r]
         # load in all addresses for random-access
         addresses = tuple(csv.DictReader(open(filename)))
         # translate all the addresses for processing
@@ -249,7 +250,7 @@ class RandomPerson:
             person = self.address_generator.next()
             # Disguise address-- change all numbers
             # First randomise numbers in first line of address
-            print(person)
+            #print(person)
             firstline = re.split("(^[0-9]+)", person[self.firstlineaddress_fld])
             # If no numbers in address, leave as is
             if len(firstline) != 1:
@@ -322,7 +323,7 @@ class RandomPerson:
             if output_filetype == 'csv':
                 # Write heading row in order of original contacts table
                 # todo-nm outgoing translations passim
-                print (self.fieldorder)
+                #print("fieldorder", self.fieldorder)
                 field_header = [fieldmap.OUTGOING_TRANSLATION[r]
                                 for r
                                 in self.fieldorder
@@ -336,7 +337,7 @@ class RandomPerson:
                     wtr.writerow(p)
                 elif output_filetype == 'django_yaml_fixture':
                     p = fieldmap.translateOut(np.next())
-                    print(p)
+                    print('map', p)
                     outputfile.write(
                         yaml.dump([{ 'model' : yaml_entity,
                                         'pk' : person_id,
